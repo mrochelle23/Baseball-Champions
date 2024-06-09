dc0198388cf2d17a7339377c0c63b9ceb8657c21
mrochelle23 <mrochellebiz@gmail.com>
2024-06-09 12:27:17 -0400

    Initial Commit

---
+++ Baseball Champions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Baseball Champions
+//
+//  Created by Mikhai Rochelle on 6/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
